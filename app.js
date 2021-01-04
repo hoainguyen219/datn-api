cc31c62d76f65df54a61bfa09b5f16f8f0be2996
hoainguyen219 <hoaint210903@gmail.com>
2021-01-04 11:27:26 +0700

    get list shcedule by post_id and check login , booking

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const admin = require('./storage')
 const config = require('./config')
 
 const { camelize } = require('./util')
-const { count } = require('./database')
+const { count, queryBuilder } = require('./database')
 
 const uploads = multer({
   limits: {
@@ -75,6 +75,7 @@ app.get('/posts', async (req, res) => {
     .sum('rating as totalScore')
     .from('post')
     .leftJoin('post_schedule', 'post.post_id', 'post_schedule.post_id')
+    .where('status', 1)
     .modify((queryBuilder) => {
       if (minArea) queryBuilder.where('area', '>=', minArea)
       if (minPrice) queryBuilder.where('price', '>=', minPrice)
@@ -107,6 +108,7 @@ app.get('/posts', async (req, res) => {
       }
     })
     .groupBy('post.post_id')
+    .limit(10)
   res.send(posts.map((x) => camelize(x)))
 })
 
@@ -148,7 +150,7 @@ app.get('/posts/:id', async (req, res) => {
     avgScore = (totalScore / totalReview).toFixed(2)
   }
   else avgScore = undefined
-  
+
   // hien thi diem danh gia nguoi cho thue
   const [{ totalReview1 }] = await knex('post_schedule')
     .count('schedule_id as totalReview1')
@@ -250,15 +252,15 @@ app.get('/schedule/:userId', async (req, res) => {
   }
 
   const rates = await knex('post_schedule')
-  .select('rating as Rating', 'post_id', 'rating_1 as Rating1')
-  .where('user_id', userId)
-  .whereNotNull('rating')
-  .whereIn('post_id', function () {
-    this.select('post_id')
-      .from('post_schedule')
-      .where('user_id', parseInt(userId))
-  })
-  .orderBy('post_id')
+    .select('rating as Rating', 'post_id', 'rating_1 as Rating1')
+    .where('user_id', userId)
+    .whereNotNull('rating')
+    .whereIn('post_id', function () {
+      this.select('post_id')
+        .from('post_schedule')
+        .where('user_id', parseInt(userId))
+    })
+    .orderBy('post_id')
 
 
   const rateMapping = []
@@ -275,7 +277,7 @@ app.get('/schedule/:userId', async (req, res) => {
   for (let schedule of schedules) {
     schedule.isRated = rateMapping[schedule.postId]
   }
-  
+
   res.send(schedules)
 
 })
@@ -328,6 +330,69 @@ app.post('/posts', async (req, res) => {
   )
   res.send({ postId: post })
 })
+
+//chinh sua
+app.patch('/posts/update/:postId', async (req, res) => {
+  console.log('kkk')
+  const postId = req.params.postId
+  const files = req.files
+  const filesUploaded = []
+  const filesUrls = []
+  const bucket = admin.storage().bucket()
+
+  if (files) {
+    const numFiles = files.length
+    for (let i = 0; i < numFiles; i++) {
+      const currentFile = files[i]
+      const time = new Date().getTime()
+      const fileName = `images/${time}-${currentFile.originalname}`
+      filesUploaded.push(bucket.file(fileName).save(currentFile.buffer))
+      filesUrls.push(
+        `https://storage.cloud.google.com/datn-a7520.appspot.com/${fileName}`
+      )
+    }
+  }
+  const newPost = req.body
+  const [post] = await knex('post').where('post_id', parseInt(postId)).update([
+    {
+      title: newPost.title,
+      area: newPost.area,
+      address: newPost.address,
+      bathroom: newPost.bathroom,
+      city: newPost.city,
+      district: newPost.district,
+      lat: newPost.lat,
+      lng: newPost.lng,
+      description: newPost.description,
+      price: newPost.price,
+      bedroom: newPost.bedroom,
+      air_condition: newPost.utilities.air_condition ? 1 : 0,
+      wc: newPost.utilities.wc ? 1 : 0,
+      garage: newPost.utilities.garage ? 1 : 0,
+      electric_water_heater: newPost.utilities.electric_water_heater ? 1 : 0,
+      status: 0,
+    },
+  ])
+  await knex('image').where('post_id', parseInt(postId)).del()
+  await knex('image').insert(
+    filesUrls.map((item) => {
+      return { url_image: item, post_id: post }
+    })
+  )
+  res.send({ postId: post })
+})
+
+//get danh sach schedule theo post_id
+app.get('/getSchedule/:postId', async(req, res) => {
+  const postId = req.params.postId
+  const listSchedule = await knex('post_schedule')
+  .select('from_date as fromDate', 'to_date as toDate')
+  .whereRaw('post_id = ? and to_date > current_date', [postId])
+
+  res.send(listSchedule)
+})
+
+
 // register
 app.post('/register', async (req, res) => {
   const user = req.body
@@ -355,14 +420,34 @@ app.post('/login', async (req, res) => {
 app.post('/posts/:id/booking', async (req, res) => {
   const postId = req.params.id
   const { fromDate, toDate, userId } = req.body
-  await knex('post_schedule').insert([
-    {
-      post_id: postId,
-      from_date: fromDate,
-      to_date: toDate,
-      user_id: userId,
-    },
-  ])
+  const booked = await knex('post_schedule')
+    .select('post_id')
+    .whereRaw(
+      `post_id = ?
+      and ((from_date between ? and ? )
+      or (to_date between ? and ?)
+      or ((? between from_date and to_date) and (? between from_date and to_date)))
+      `,
+      [postId, fromDate, toDate, fromDate, toDate, fromDate, toDate])
+    .andWhere(function() {
+      this.where('post_id', parseInt(postId))
+    })
+  console.log(365, booked.length)
+  console.log(366, booked)
+  if (booked.length > 0) {
+    res.sendStatus(400)
+    // res.send('Đặt lịch không thành công: Trùng lịch')
+  }
+  else {
+    await knex('post_schedule').insert([
+      {
+        post_id: postId,
+        from_date: fromDate,
+        to_date: toDate,
+        user_id: userId,
+      },
+    ])
+  }
   res.send({ postId })
 })
 
@@ -390,7 +475,7 @@ app.get('/admin/tk', async (req, res) => {
     .count('city as count')
     .groupBy('city')
     .orderBy('count', 'desc')
-    .limit(10)
+    // .limit(10)
   let totalPost = await knex('post').count('* as totalPost').first()
 
   totalPost.cities = cities
@@ -432,6 +517,16 @@ app.post('/admin/accept/:postId', async (req, res) => {
   if (!post) return res.sendStatus(404)
   return res.send({ postId })
 })
+
+//phân trang
+app.get('/news/:page', async (req, res, next) => {
+  let perPage = 15; // số lượng sản phẩm xuất hiện trên 1 page
+  let page = req.params.page || 1;
+  const products = await knex('post').select('*')
+    .offset((perPage * page) - perPage) // Trong page đầu tiên sẽ bỏ qua giá trị là 0
+    .limit(perPage)
+  res.send(products)
+});
 
 const port = process.env.PORT || 5000
 const host = process.env.HOST || 'localhost'
